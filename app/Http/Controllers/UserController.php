35bf76476da458d325bb73f92ec12f3f8ea9edaf
agungsapp <agung.dni19@gmail.com>
2024-10-26 10:04:59 +0700

    initial project

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PDO;
+
+class UserController extends Controller
+{
+    public function home()
+    {
+        return "halaman home";
+    }
+}
